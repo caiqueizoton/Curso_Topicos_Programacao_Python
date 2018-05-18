82f721303d9df3a85c672c54875224051c096614
caiqueizoton <39201311+caiqueizoton@users.noreply.github.com>
2018-05-18 18:58:03 -0300

    Delete Tabuada.py

--- Tabuada.py
+++
@@ -1,8 +0,0 @@
-# programa que gera a tabuada de multiplicação
-
-base = int(input("Digite o número base da tabuada:  "))
-mult = 10
-
-while mult >= 0:
-    print (f"O valor de {base} vezes {mult} é :{base*mult}")
-    mult -= 1
