57c00c3d8daae85bab3d4f32279b125dadb93964
carlos henrique Izoton <caique@colan96.cefet-rj.br>
2018-05-18 19:08:13 -0300

    Exercícios da segunda aula

---
+++ aula2/contador foguete.py
@@ -0,0 +1,11 @@
+# programa que ao receber um número de usuário imprime todos os números de 0 até o fornecido
+
+x = int(input("Digite 10 para iniciar a contagem  "))
+y = 0
+
+while x >= y:
+    print(x)
+    x -= 1
+print ('Fogo!')
+
+
